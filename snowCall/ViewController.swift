b3b6bdfc5b18b34d3863a227839b33d7cf7da002
lcs-JZhang <XZhang@lcs.on.ca>
2019-11-28 14:47:07 -0500

    ui finish

--- snowCall/ViewController.swift
+++ snowCall/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextView!
+    
+    @IBOutlet weak var output: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func convert(_ sender: Any) {
+    }
+    
 }
 
