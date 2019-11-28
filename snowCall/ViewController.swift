620357305764c0981707007a4b1fb2d999db66c9
lcs-JZhang <XZhang@lcs.on.ca>
2019-11-28 14:26:09 -0500

    Initial Commit

---
+++ snowCall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snowCall
+//
+//  Created by Jesse Zhang on 2019-11-28.
+//  Copyright © 2019 Jesse Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
